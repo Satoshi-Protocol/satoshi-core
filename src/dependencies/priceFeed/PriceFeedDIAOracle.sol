b7dfc2bb852ef10103d17e307b55c07510f1cb4c
ashirleyshe <ashirleyshe@gmail.com>
2024-02-17 18:16:12 +0800

    add: check last update time when fetch price from DIA oracle

--- src/dependencies/priceFeed/PriceFeedDIAOracle.sol
+++ src/dependencies/priceFeed/PriceFeedDIAOracle.sol
@@ -3,25 +3,40 @@ pragma solidity 0.8.13;
 
 import {IPriceFeed} from "../../interfaces/dependencies/IPriceFeed.sol";
 import {IDIAOracleV2} from "../../interfaces/dependencies/priceFeed/IDIAOracleV2.sol";
+import {SatoshiOwnable} from "../SatoshiOwnable.sol";
 
-contract PriceFeedDIAOracle is IPriceFeed {
+contract PriceFeedDIAOracle is IPriceFeed, SatoshiOwnable {
     IDIAOracleV2 internal immutable _source;
     uint8 internal immutable _decimals;
     string internal _key;
+    uint256 public diaMaxTimeThreshold;
 
     constructor(IDIAOracleV2 source_, uint8 decimals_, string memory key_) {
         _source = source_;
         _decimals = decimals_;
         _key = key_;
+        diaMaxTimeThreshold = 86400;
     }
 
     function fetchPrice() external returns (uint256) {
-        (uint128 price,) = _source.getValue(_key);
+        (uint128 price, uint128 lastUpdated) = _source.getValue(_key);
         if (price == 0) revert InvalidPriceUInt128(price);
+        if (block.timestamp - lastUpdated > diaMaxTimeThreshold) {
+            revert PriceTooOld();
+        }
         return uint256(price);
     }
 
     function decimals() external view returns (uint8) {
         return _decimals;
+    }
+
+    function updateDIAParams(uint128 _maxTime) public onlyOwner {
+        if (_maxTime <= 120) {
+            revert InvalidTime();
+        }
+
+        diaMaxTimeThreshold = _maxTime;
+        emit DIAParamsUpdated(_maxTime);
     }
 }
--- src/interfaces/dependencies/IPriceFeed.sol
+++ src/interfaces/dependencies/IPriceFeed.sol
@@ -5,6 +5,11 @@ interface IPriceFeed {
     // invalid price error for different types of price sources
     error InvalidPriceInt256(int256 price);
     error InvalidPriceUInt128(uint128 price);
+    error PriceTooOld();
+    error InvalidTime();
+
+    // Events
+    event DIAParamsUpdated(uint128 diaMaxTimeThreshold);
 
     function fetchPrice() external returns (uint256);
 
