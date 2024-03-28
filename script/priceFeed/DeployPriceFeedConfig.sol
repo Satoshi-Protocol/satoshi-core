9a9a6f8f23dfd27f7a720ecf2acc139dac58ea4c
Ray Huang <ray880301@gmail.com>
2024-03-28 22:05:36 +0800

    update config

--- script/priceFeed/DeployPriceFeedConfig.sol
+++ script/priceFeed/DeployPriceFeedConfig.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 /* Satoshi Core Address */
-address constant SATOSHI_CORE_ADDRESS = 0xdF4c52C6be46673210C12262e09f8Ae417E7Beff;
+address constant SATOSHI_CORE_ADDRESS = 0x365b4915289f2b27dcA58BEBd2960ECDCC2AE3b6;
 
 /* Chainlink Integration Config */
 //NOTE: chainlink price feed source address
@@ -10,7 +10,7 @@ address constant CHAINLINK_PRICE_FEED_SOURCE_ADDRESS = 0x4c99AD68C293A9de0Cc04B8
 
 /* DIA Oracle Integration Config */
 //NOTE: DIA oracle source address
-address constant DIA_ORACLE_PRICE_FEED_SOURCE_ADDRESS = 0xDfAA53cB0Ce891485389fEBdcD547965906A8300;
+address constant DIA_ORACLE_PRICE_FEED_SOURCE_ADDRESS = 0x9a9a5113b853b394E9BA5FdB7e72bC5797C85191;
 uint8 constant DIA_ORACLE_PRICE_FEED_DECIMALS = 8;
 string constant DIA_ORACLE_PRICE_FEED_KEY = "BTC/USD";
 
