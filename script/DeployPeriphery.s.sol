d950afa7602e99187f779b7d3b82cad238c3d576
ashirleyshe <ashirleyshe@gmail.com>
2024-05-19 01:01:06 +0800

    add deploy periphery script

--- script/DeployPeriphery.s.sol
+++ script/DeployPeriphery.s.sol
@@ -12,9 +12,9 @@ import {SatoshiPeriphery} from "../src/helpers/SatoshiPeriphery.sol";
 contract DeploySatoshiPeripheryScript is Script {
     uint256 internal DEPLOYMENT_PRIVATE_KEY;
     ISatoshiPeriphery satoshiPeriphery;
-    IDebtToken debtToken = IDebtToken(0x1d015247bD90b92727A66Eae18608E34b8693487);
-    IBorrowerOperations borrowerOperationsProxy = IBorrowerOperations(0x0EE5326B901deEe0e47E620533d94a8055a7A97B);
-    address constant WETH_ADDRESS = 0x4200000000000000000000000000000000000006;
+    IDebtToken debtToken = IDebtToken(0xc41BC99759Eaf63467FDAe72c209C652647C6f95);
+    IBorrowerOperations borrowerOperationsProxy = IBorrowerOperations(0x365b4915289f2b27dcA58BEBd2960ECDCC2AE3b6);
+    address constant WETH_ADDRESS = 0x2DcA0825F0d5E900c1522a9A2362237BbaAecbb4;
 
     function setUp() public {
         DEPLOYMENT_PRIVATE_KEY = uint256(vm.envBytes32("DEPLOYMENT_PRIVATE_KEY"));
