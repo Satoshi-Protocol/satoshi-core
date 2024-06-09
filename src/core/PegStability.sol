31cc7cde9f711177fae6cbc52eee060f6b7114f1
ashirleyshe <ashirleyshe@gmail.com>
2024-06-09 22:01:06 +0800

    add psm

---
+++ src/core/PegStability.sol
@@ -0,0 +1,424 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.19;
+
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import {SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import {IERC20MetadataUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IDebtToken} from "../interfaces/core/IDebtToken.sol";
+import {IPegStability} from "../interfaces/core/IPegStability.sol";
+import {IPriceFeedAggregator} from "../interfaces/core/IPriceFeedAggregator.sol";
+import {ISatoshiCore} from "../interfaces/core/ISatoshiCore.sol";
+
+import {SatoshiOwnable} from "../dependencies/SatoshiOwnable.sol";
+
+/**
+ * @title Peg Stability Module Contract.
+ * Mutated from:
+ * https://github.com/VenusProtocol/venus-protocol/blob/develop/contracts/PegStability/PegStability.sol
+ * @notice Contract for swapping stable token for SAT token and vice versa to maintain the peg stability between them.
+ */
+
+contract PegStability is IPegStability, SatoshiOwnable, ReentrancyGuardUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    uint256 public constant TARGET_DIGITS = 18;
+    
+    /// @notice The divisor used to convert fees to basis points.
+    uint256 public constant BASIS_POINTS_DIVISOR = 10000;
+
+    /// @notice The mantissa value representing 1 (used for calculations).
+    uint256 public constant MANTISSA_ONE = 1e18;
+
+    /// @notice The value representing one dollar in the stable token.
+    /// @dev Our oracle is returning amount depending on the number of decimals of the stable token. (36 - asset_decimals) E.g. 8 decimal asset = 1e28.
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    uint256 public immutable ONE_DOLLAR;
+
+    IDebtToken immutable public SAT;
+
+    /// @notice The address of the stable token contract.
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    address public immutable STABLE_TOKEN_ADDRESS;
+
+    /// @notice The address of ResilientOracle contract wrapped in its interface.
+    IPriceFeedAggregator public oracle;
+
+    /// @notice The address of the Reward Manager.
+    address public rewardManagerAddr;
+
+    /// @notice The incoming stableCoin fee. (Fee for swapStableForSAT).
+    uint256 public feeIn;
+
+    /// @notice The outgoing stableCoin fee. (Fee for swapSATForStable).
+    uint256 public feeOut;
+
+    /// @notice The maximum amount of SAT that can be minted through this contract.
+    uint256 public satMintCap;
+
+    /// @notice The total amount of SAT minted through this contract.
+    uint256 public satMinted;
+
+    /// @notice A flag indicating whether the contract is currently paused or not.
+    bool public isPaused;
+
+    /// @notice A flag indicating whether the contract is using an oracle or not.
+    bool public usingOracle;
+
+    /**
+     * @dev Prevents functions to execute when contract is paused.
+     */
+    modifier isActive() {
+        if (isPaused) revert Paused();
+        _;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(address stableTokenAddress_, address SATAddress_) {
+        _ensureNonzeroAddress(stableTokenAddress_);
+        _ensureNonzeroAddress(SATAddress_);
+
+        ONE_DOLLAR = 10 ** 18; // 1$ scaled to the decimals returned by our Oracle
+        STABLE_TOKEN_ADDRESS = stableTokenAddress_;
+        SAT = IDebtToken(SATAddress_);
+        _disableInitializers();
+    }
+
+    /**
+     * @notice Initializes the contract via Proxy Contract with the required parameters.
+     * @param rewardManagerAddr_ The address where fees will be sent.
+     * @param oracleAddress_ The address of the ResilientOracle contract.
+     * @param feeIn_ The percentage of fees to be applied to a stablecoin -> SAT swap.
+     * @param feeOut_ The percentage of fees to be applied to a SAT -> stablecoin swap.
+     * @param satMintCap_ The cap for the total amount of SAT that can be minted.
+     */
+    function initialize(
+        ISatoshiCore satoshiCore_,
+        address rewardManagerAddr_,
+        address oracleAddress_,
+        uint256 feeIn_,
+        uint256 feeOut_,
+        uint256 satMintCap_
+    ) external initializer {
+        __SatoshiOwnable_init(satoshiCore_);
+        __ReentrancyGuard_init();
+
+        if (feeIn_ >= BASIS_POINTS_DIVISOR || feeOut_ >= BASIS_POINTS_DIVISOR) {
+            revert InvalidFee();
+        }
+
+        feeIn = feeIn_;
+        feeOut = feeOut_;
+        satMintCap = satMintCap_;
+        rewardManagerAddr = rewardManagerAddr_;
+        oracle = IPriceFeedAggregator(oracleAddress_);
+    }
+
+    /*** Swap Functions ***/
+
+    /**
+     * @notice Swaps SAT for a stable token.
+     * @param receiver The address where the stablecoin will be sent.
+     * @param stableTknAmount The amount of stable tokens to receive.
+     * @return The amount of SAT received and burnt from the sender.
+     */
+    // @custom:event Emits SATForStableSwapped event.
+    function swapSATForStable(
+        address receiver,
+        uint256 stableTknAmount
+    ) external isActive nonReentrant returns (uint256) {
+        _ensureNonzeroAddress(receiver);
+        _ensureNonzeroAmount(stableTknAmount);
+
+        // dec 18
+        uint256 stableTknAmountUSD = _previewTokenUSDAmount(stableTknAmount);
+        uint256 fee = _calculateFee(stableTknAmountUSD, FeeDirection.OUT);
+
+        if (SAT.balanceOf(msg.sender) < stableTknAmountUSD + fee) {
+            revert NotEnoughSAT();
+        }
+        if (satMinted < stableTknAmountUSD) {
+            revert SATMintedUnderflow();
+        }
+
+        unchecked {
+            satMinted -= stableTknAmountUSD;
+        }
+
+        if (fee != 0) {
+            bool success = SAT.transferFrom(msg.sender, rewardManagerAddr, fee);
+            if (!success) {
+                revert SATTransferFail();
+            }
+        }
+
+        SAT.burn(msg.sender, stableTknAmountUSD);
+        IERC20Upgradeable(STABLE_TOKEN_ADDRESS).safeTransfer(receiver, stableTknAmount);
+        emit SATForStableSwapped(stableTknAmountUSD, stableTknAmount, fee);
+        return stableTknAmountUSD;
+    }
+
+    /**
+     * @notice Swaps stable tokens for SAT with fees.
+     * @dev This function adds support to fee-on-transfer tokens. The actualTransferAmt is calculated, by recording token balance state before and after the transfer.
+     * @param receiver The address that will receive the SAT tokens.
+     * @param stableTknAmount The amount of stable tokens to be swapped.
+     * @return Amount of SAT minted to the sender.
+     */
+    // @custom:event Emits StableForSATSwapped event.
+    function swapStableForSAT(
+        address receiver,
+        uint256 stableTknAmount
+    ) external isActive nonReentrant returns (uint256) {
+        _ensureNonzeroAddress(receiver);
+        _ensureNonzeroAmount(stableTknAmount);
+        // transfer IN, supporting fee-on-transfer tokens
+        uint256 balanceBefore = IERC20Upgradeable(STABLE_TOKEN_ADDRESS).balanceOf(address(this));
+        IERC20Upgradeable(STABLE_TOKEN_ADDRESS).safeTransferFrom(msg.sender, address(this), stableTknAmount);
+        uint256 balanceAfter = IERC20Upgradeable(STABLE_TOKEN_ADDRESS).balanceOf(address(this));
+
+        // calculate actual transfered amount (in case of fee-on-transfer tokens)
+        uint256 actualTransferAmt = balanceAfter - balanceBefore;
+
+        // convert to decimal 18
+        uint256 actualTransferAmtInUSD = _previewTokenUSDAmount(actualTransferAmt);
+
+        // calculate feeIn
+        uint256 fee = _calculateFee(actualTransferAmtInUSD, FeeDirection.IN);
+        uint256 SATToMint = actualTransferAmtInUSD - fee;
+
+        if (satMinted + actualTransferAmtInUSD > satMintCap) {
+            revert SATMintCapReached();
+        }
+        unchecked {
+            satMinted += actualTransferAmtInUSD;
+        }
+
+        // mint SAT to receiver
+        SAT.mint(receiver, SATToMint);
+
+        // mint SAT fee to venus treasury
+        if (fee != 0) {
+            SAT.mint(rewardManagerAddr, fee);
+        }
+
+        emit StableForSATSwapped(actualTransferAmt, SATToMint, fee);
+        return SATToMint;
+    }
+
+    /*** Admin Functions ***/
+
+    /**
+     * @notice Pause the PSM contract.
+     * @dev Reverts if the contract is already paused.
+     */
+    // @custom:event Emits PSMPaused event.
+    function pause() external onlyOwner {
+        if (isPaused) {
+            revert AlreadyPaused();
+        }
+        isPaused = true;
+        emit PSMPaused(msg.sender);
+    }
+
+    /**
+     * @notice Resume the PSM contract.
+     * @dev Reverts if the contract is not paused.
+     */
+    // @custom:event Emits PSMResumed event.
+    function resume() external onlyOwner {
+        if (!isPaused) {
+            revert NotPaused();
+        }
+        isPaused = false;
+        emit PSMResumed(msg.sender);
+    }
+
+    /**
+     * @notice Set the fee percentage for incoming swaps.
+     * @dev Reverts if the new fee percentage is invalid (greater than or equal to BASIS_POINTS_DIVISOR).
+     * @param feeIn_ The new fee percentage for incoming swaps.
+     */
+    // @custom:event Emits FeeInChanged event.
+    function setFeeIn(uint256 feeIn_) external onlyOwner {
+        // feeIn = 10000 = 100%
+        if (feeIn_ >= BASIS_POINTS_DIVISOR) {
+            revert InvalidFee();
+        }
+        uint256 oldFeeIn = feeIn;
+        feeIn = feeIn_;
+        emit FeeInChanged(oldFeeIn, feeIn_);
+    }
+
+    /**
+     * @notice Set the fee percentage for outgoing swaps.
+     * @dev Reverts if the new fee percentage is invalid (greater than or equal to BASIS_POINTS_DIVISOR).
+     * @param feeOut_ The new fee percentage for outgoing swaps.
+     */
+    // @custom:event Emits FeeOutChanged event.
+    function setFeeOut(uint256 feeOut_) external onlyOwner {
+        // feeOut = 10000 = 100%
+        if (feeOut_ >= BASIS_POINTS_DIVISOR) {
+            revert InvalidFee();
+        }
+        uint256 oldFeeOut = feeOut;
+        feeOut = feeOut_;
+        emit FeeOutChanged(oldFeeOut, feeOut_);
+    }
+
+    /**
+     * @dev Set the maximum amount of SAT that can be minted through this contract.
+     * @param satMintCap_ The new maximum amount of SAT that can be minted.
+     */
+    // @custom:event Emits SATMintCapChanged event.
+    function setSATMintCap(uint256 satMintCap_) external onlyOwner {
+        uint256 oldsatMintCap = satMintCap;
+        satMintCap = satMintCap_;
+        emit SATMintCapChanged(oldsatMintCap, satMintCap_);
+    }
+
+    function setRewardManager(address rewardManager_) external onlyOwner {
+        address oldTreasuryAddress = rewardManagerAddr;
+        rewardManagerAddr = rewardManager_;
+        emit RewardManagerChanged(oldTreasuryAddress, rewardManager_);
+    }
+
+    function setUsingOracle(bool usingOracle_) external onlyOwner {
+        usingOracle = usingOracle_;
+    }
+
+    /**
+     * @notice Set the address of the ResilientOracle contract.
+     * @dev Reverts if the new address is zero.
+     * @param oracleAddress_ The new address of the ResilientOracle contract.
+     */
+    // @custom:event Emits OracleChanged event.
+    function setOracle(address oracleAddress_) external onlyOwner {
+        _ensureNonzeroAddress(oracleAddress_);
+        address oldOracleAddress = address(oracle);
+        oracle = IPriceFeedAggregator(oracleAddress_);
+        emit OracleChanged(oldOracleAddress, oracleAddress_);
+    }
+
+    function transerToken(address token, uint256 amount) external onlyOwner {
+        IERC20(token).transfer(msg.sender, amount);
+    }
+
+    /*** Helper Functions ***/
+
+    /**
+     * @notice Calculates the amount of SAT that would be burnt from the user.
+     * @dev This calculation might be off with a bit, if the price of the oracle for this asset is not updated in the block this function is invoked.
+     * @param stableTknAmount The amount of stable tokens to be received after the swap.
+     * @return The amount of SAT that would be taken from the user.
+     */
+    function previewSwapSATForStable(uint256 stableTknAmount) external returns (uint256) {
+        _ensureNonzeroAmount(stableTknAmount);
+        uint256 stableTknAmountUSD = _previewTokenUSDAmount(stableTknAmount);
+        uint256 fee = _calculateFee(stableTknAmountUSD, FeeDirection.OUT);
+
+        if (satMinted < stableTknAmountUSD) {
+            revert SATMintedUnderflow();
+        }
+
+        return stableTknAmountUSD + fee;
+    }
+
+    /**
+     * @notice Calculates the amount of SAT that would be sent to the receiver.
+     * @dev This calculation might be off with a bit, if the price of the oracle for this asset is not updated in the block this function is invoked.
+     * @param stableTknAmount The amount of stable tokens provided for the swap.
+     * @return The amount of SAT that would be sent to the receiver.
+     */
+    function previewSwapStableForSAT(uint256 stableTknAmount) external returns (uint256) {
+        _ensureNonzeroAmount(stableTknAmount);
+        uint256 stableTknAmountUSD = _previewTokenUSDAmount(stableTknAmount);
+
+        //calculate feeIn
+        uint256 fee = _calculateFee(stableTknAmountUSD, FeeDirection.IN);
+        uint256 SATToMint = stableTknAmountUSD - fee;
+
+        if (satMinted + stableTknAmountUSD > satMintCap) {
+            revert SATMintCapReached();
+        }
+
+        return SATToMint;
+    }
+
+    /**
+     * @dev Calculates the USD value of the given amount of stable tokens depending on the swap direction.
+     * @param amount The amount of stable tokens.
+     * @return The USD value of the given amount of stable tokens scaled by 1e18 taking into account the direction of the swap
+     */
+    function _previewTokenUSDAmount(uint256 amount) internal returns (uint256) {
+        return (_getScaledAmt(amount) * _getPriceInUSD()) / MANTISSA_ONE;
+    }
+
+    function _getScaledAmt(uint256 amount) internal view returns (uint256) {
+        uint256 scaledAmt;
+        uint256 decimals = IERC20MetadataUpgradeable(STABLE_TOKEN_ADDRESS).decimals();
+        if (decimals == TARGET_DIGITS) {
+            scaledAmt = amount;
+        } else if (decimals < TARGET_DIGITS) {
+            scaledAmt = amount * (10 ** (TARGET_DIGITS - decimals));
+        } else {
+            scaledAmt = amount / (10 ** (decimals - TARGET_DIGITS));
+        }
+
+        return scaledAmt;
+    }
+
+    /**
+     * @notice Get the price of stable token in USD.
+     * @dev This function gets the price of the stable token in USD.
+     * @return The price in USD, adjusted based on the selected direction.
+     */
+    function _getPriceInUSD() internal returns (uint256) {
+        // fetch price with decimal 18
+        uint256 price = oracle.fetchPrice(IERC20(STABLE_TOKEN_ADDRESS));
+
+        return usingOracle ? price : ONE_DOLLAR;
+    }
+
+    /**
+     * @notice Calculate the fee amount based on the input amount and fee percentage.
+     * @dev Reverts if the fee percentage calculation results in rounding down to 0.
+     * @param amount The input amount to calculate the fee from.
+     * @param direction The direction of the fee: FeeDirection.IN or FeeDirection.OUT.
+     * @return The fee amount.
+     */
+    function _calculateFee(uint256 amount, FeeDirection direction) internal view returns (uint256) {
+        uint256 feePercent;
+        if (direction == FeeDirection.IN) {
+            feePercent = feeIn;
+        } else {
+            feePercent = feeOut;
+        }
+        if (feePercent == 0) {
+            return 0;
+        } else {
+            // checking if the percent calculation will result in rounding down to 0
+            if (amount * feePercent < BASIS_POINTS_DIVISOR) {
+                revert AmountTooSmall();
+            }
+            return (amount * feePercent) / BASIS_POINTS_DIVISOR;
+        }
+    }
+
+    /**
+     * @notice Checks that the address is not the zero address.
+     * @param someone The address to check.
+     */
+    function _ensureNonzeroAddress(address someone) private pure {
+        if (someone == address(0)) revert ZeroAddress();
+    }
+
+    /**
+     * @notice Checks that the amount passed as stable tokens is bigger than zero
+     * @param amount The amount to validate
+     */
+    function _ensureNonzeroAmount(uint256 amount) private pure {
+        if (amount == 0) revert ZeroAmount();
+    }
+}
---
+++ src/interfaces/core/IPegStability.sol
@@ -0,0 +1,145 @@
+pragma solidity ^0.8.19;
+
+import {IDebtToken} from "./IDebtToken.sol";
+import {IPriceFeedAggregator} from "./IPriceFeedAggregator.sol";
+import {ISatoshiCore} from "./ISatoshiCore.sol";
+
+interface IPegStability {
+
+    // Helper enum for calculation of the fee.
+    enum FeeDirection {
+        IN,
+        OUT
+    }
+
+    /// @notice Event emitted when contract is paused.
+    event PSMPaused(address indexed admin);
+
+    /// @notice Event emitted when the contract is resumed after pause.
+    event PSMResumed(address indexed admin);
+
+    /// @notice Event emitted when feeIn state var is modified.
+    event FeeInChanged(uint256 oldFeeIn, uint256 newFeeIn);
+
+    /// @notice Event emitted when feeOut state var is modified.
+    event FeeOutChanged(uint256 oldFeeOut, uint256 newFeeOut);
+
+    /// @notice Event emitted when SATMintCap state var is modified.
+    event SATMintCapChanged(uint256 oldCap, uint256 newCap);
+
+    /// @notice Event emitted when RewardManager state var is modified.
+    event RewardManagerChanged(address indexed oldTreasury, address indexed newTreasury);
+
+    /// @notice Event emitted when oracle state var is modified.
+    event OracleChanged(address indexed oldOracle, address indexed newOracle);
+
+    /// @notice Event emitted when stable token is swapped for SAT.
+    event StableForSATSwapped(uint256 stableIn, uint256 SATOut, uint256 fee);
+
+    /// @notice Event emitted when stable token is swapped for SAT.
+    event SATForStableSwapped(uint256 SATBurnt, uint256 stableOut, uint256 SATFee);
+
+    /// @notice thrown when contract is in paused state
+    error Paused();
+
+    /// @notice thrown when attempted to pause an already paused contract
+    error AlreadyPaused();
+
+    /// @notice thrown when attempted to resume the contract if it is already resumed
+    error NotPaused();
+
+    /// @notice thrown when stable token has more than 18 decimals
+    error TooManyDecimals();
+
+    /// @notice thrown when fee is >= 100%
+    error InvalidFee();
+
+    /// @notice thrown when a zero address is passed as a function parameter
+    error ZeroAddress();
+
+    /// @notice thrown when a zero amount is passed as stable token amount parameter
+    error ZeroAmount();
+
+    /// @notice thrown when the user doesn't have enough SAT balance to provide for the amount of stable tokens he wishes to get
+    error NotEnoughSAT();
+
+    /// @notice thrown when the amount of SAT to be burnt exceeds the SATMinted amount
+    error SATMintedUnderflow();
+
+    /// @notice thrown when the SAT transfer to treasury fails
+    error SATTransferFail();
+
+    /// @notice thrown when SAT to be minted will go beyond the mintCap threshold
+    error SATMintCapReached();
+
+    /// @notice thrown when fee calculation will result in rounding down to 0 due to stable token amount being a too small number
+    error AmountTooSmall();
+
+    function TARGET_DIGITS() external view returns (uint256);
+
+    function BASIS_POINTS_DIVISOR() external view returns (uint256);
+
+    function MANTISSA_ONE() external view returns (uint256);
+
+    function ONE_DOLLAR() external view returns (uint256);
+
+    function SAT() external view returns (IDebtToken);
+
+    function STABLE_TOKEN_ADDRESS() external view returns (address);
+
+    function oracle() external view returns (IPriceFeedAggregator);
+
+    function rewardManagerAddr() external view returns (address);
+
+    function feeIn() external view returns (uint256);
+
+    function feeOut() external view returns (uint256);
+
+    function satMintCap() external view returns (uint256);
+
+    function isPaused() external view returns (bool);   
+
+    function usingOracle() external view returns (bool);
+
+    function initialize(
+        ISatoshiCore satoshiCore_,
+        address rewardManagerAddr_,
+        address oracleAddress_,
+        uint256 feeIn_,
+        uint256 feeOut_,
+        uint256 satMintCap_
+    ) external;
+
+    function swapSATForStable(
+        address receiver,
+        uint256 stableTknAmount
+    ) external returns (uint256);
+
+    function swapStableForSAT(
+        address receiver,
+        uint256 stableTknAmount
+    ) external returns (uint256);
+
+    function pause() external;
+
+    function resume() external;
+
+    function setFeeIn(uint256 newFeeIn) external;
+
+    function setFeeOut(uint256 newFeeOut) external;
+
+    function setSATMintCap(uint256 newCap) external;
+
+    function setRewardManager(address rewardManager_) external;
+
+    function setUsingOracle(bool usingOracle_) external;
+
+    function setOracle(address oracle_) external;
+
+    function transerToken(address token, uint256 amount) external;
+
+    function previewSwapSATForStable(uint256 stableTknAmount) external returns (uint256);
+
+    function previewSwapStableForSAT(uint256 stableTknAmount) external returns (uint256);
+}
+
