ce9f7b6165df0de210d497bab9848b7486fe90e2
ashirleyshe <ashirleyshe@gmail.com>
2024-07-16 00:38:11 +0800

    add nym deploy script

--- script/DeployNYM.s.sol
+++ script/DeployNYM.s.sol
@@ -6,13 +6,15 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 import {NexusYieldManager} from "../src/core/NexusYieldManager.sol";
 import {INexusYieldManager} from "../src/interfaces/core/INexusYieldManager.sol";
 import {ISatoshiCore} from "../src/interfaces/core/ISatoshiCore.sol";
+import {ASSET, FEE_IN, FEE_OUT, MINT_CAP, DAILY_MINT_CAP, ORACLE, USING_ORACLE, SWAP_WAIT_TIME} from "./DeployNYMConfig.sol";
 
 contract DeployNYMScript is Script {
     uint256 internal OWNER_PRIVATE_KEY;
     uint256 internal DEPLOYMENT_PRIVATE_KEY;
-    address constant debtTokenAddr = 0x9f5bCD3529d3B610BeA2b4E5FE273D1Fc059B8F6;
-    ISatoshiCore satoshiCore = ISatoshiCore(0x04d4a0295a7e15941cfDfFbcC5EE97cc1f347CF0);
-    address constant rewardManagerProxy = 0xc5624B51890A66E0e36Ab9DbC98a810FD7507da4;
+    address constant debtTokenAddr = 0x942f2071a9A567c5b153b6fbAEB2deAf5cE76208;
+    ISatoshiCore satoshiCore = ISatoshiCore(0xA8f683335A38048a82739cd5a996150f1c91B8C1);
+    address constant rewardManagerProxy = 0x269EeCEa5E9304fA1bc9361461798134e9AE4A60;
+    INexusYieldManager nym;
 
     function setUp() public {
         OWNER_PRIVATE_KEY = uint256(vm.envBytes32("OWNER_PRIVATE_KEY"));
@@ -26,10 +28,20 @@ contract DeployNYMScript is Script {
 
         bytes memory data = abi.encodeCall(INexusYieldManager.initialize, (satoshiCore, address(rewardManagerProxy)));
 
-        INexusYieldManager proxy = INexusYieldManager(address(new ERC1967Proxy(address(nexusYieldImpl), data)));
+        nym = INexusYieldManager(address(new ERC1967Proxy(address(nexusYieldImpl), data)));
         console.log("NexusYieldManagerImpl:", address(nexusYieldImpl));
-        console.log("NexusYieldManager:", address(proxy));
+        console.log("NexusYieldManager:", address(nym));
 
         vm.stopBroadcast();
+        
+        vm.startBroadcast(OWNER_PRIVATE_KEY);
+        
+        _setAssetConfig();
+
+        vm.stopBroadcast();
+    }
+
+    function _setAssetConfig() internal {
+        nym.setAssetConfig(ASSET, FEE_IN, FEE_OUT, MINT_CAP, DAILY_MINT_CAP, ORACLE, USING_ORACLE, SWAP_WAIT_TIME);
     }
 }
