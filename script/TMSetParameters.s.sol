750c97008f9527d91ebecf48397635101589d7fd
ashirleyshe <ashirleyshe@gmail.com>
2024-07-24 00:16:43 +0800

    update config

---
+++ script/TMSetParameters.s.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ITroveManager} from "../src/interfaces/core/ITroveManager.sol";
+import {DeploymentParams} from "../src/interfaces/core/IFactory.sol";
+import {TroveHelper} from "../src/helpers/TroveHelper.sol";
+import {
+    MINUTE_DECAY_FACTOR,
+    REDEMPTION_FEE_FLOOR,
+    MAX_REDEMPTION_FEE,
+    BORROWING_FEE_FLOOR,
+    MAX_BORROWING_FEE,
+    INTEREST_RATE_IN_BPS,
+    MAX_DEBT,
+    MCR,
+    REWARD_RATE,
+    TM_ALLOCATION,
+    TM_CLAIM_START_TIME
+} from "../script/DeployInstanceConfig.sol";
+
+contract TMSetParamtersScript is Script {
+    ITroveManager troveManagerstBTC = ITroveManager(0xe9897fe6C8bf96D5ef8B0ECC7cBfEdef9818232c);
+    DeploymentParams internal params;
+    uint256 internal OWNER_PRIVATE_KEY;
+
+    function setUp() public {
+        OWNER_PRIVATE_KEY = uint256(vm.envBytes32("OWNER_PRIVATE_KEY"));
+    }
+
+    function run() public {
+        vm.startBroadcast(OWNER_PRIVATE_KEY);
+        params = DeploymentParams({
+            minuteDecayFactor: MINUTE_DECAY_FACTOR,
+            redemptionFeeFloor: REDEMPTION_FEE_FLOOR,
+            maxRedemptionFee: MAX_REDEMPTION_FEE,
+            borrowingFeeFloor: BORROWING_FEE_FLOOR,
+            maxBorrowingFee: MAX_BORROWING_FEE,
+            interestRateInBps: 0,
+            maxDebt: MAX_DEBT,
+            MCR: MCR,
+            rewardRate: REWARD_RATE,
+            OSHIAllocation: TM_ALLOCATION,
+            claimStartTime: TM_CLAIM_START_TIME 
+        });
+
+        troveManagerstBTC.setParameters(
+            params.minuteDecayFactor,
+            params.redemptionFeeFloor,
+            params.maxRedemptionFee,
+            params.borrowingFeeFloor,
+            params.maxBorrowingFee,
+            params.interestRateInBps,
+            params.maxDebt,
+            params.MCR,
+            params.rewardRate,
+            params.claimStartTime
+        );
+
+        assert(troveManagerstBTC.MCR() == 12 * 1e17);
+        vm.stopBroadcast();
+    }
+}
