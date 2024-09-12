0eedc9b3e1b2bc7f68ecf982bd9943969f059be2
ashirleyshe <ashirleyshe@gmail.com>
2024-09-12 12:19:17 +0800

    add debtToken upgrade script

---
+++ script/UpgradeDebtToken.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DebtToken} from "../src/core/DebtToken.sol";
+import {IDebtToken} from "../src/interfaces/core/IDebtToken.sol";
+
+contract UpgradeDebtTokenScript is Script {
+    uint256 internal OWNER_PRIVATE_KEY;
+    address constant debtTokenAddr = 0x78Fea795cBFcC5fFD6Fb5B845a4f53d25C283bDB;
+    string constant DEBT_TOKEN_NAME = "Satoshi Stablecoin";
+    string constant DEBT_TOKEN_SYMBOL = "satUSD";
+
+    function setUp() public {
+        OWNER_PRIVATE_KEY = uint256(vm.envBytes32("OWNER_PRIVATE_KEY"));
+    }
+
+    function run() public {
+        vm.startBroadcast(OWNER_PRIVATE_KEY);
+
+        DebtToken debtTokenImpl = new DebtToken();
+
+        DebtToken debtTokenProxy = DebtToken(debtTokenAddr);
+
+        debtTokenProxy.upgradeTo(address(debtTokenImpl));
+
+        console.log(debtTokenProxy.name());
+        console.log(debtTokenProxy.symbol());
+
+        console.log("new Impl is deployed at", address(debtTokenProxy));
+
+        vm.stopBroadcast();
+    }
+}
